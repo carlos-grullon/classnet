29e9037a6b0f57025a7107cbb05324db8959ddba
alexioficial <grullonmatiasalexi@gmail.com>
2025-05-05 15:32:23 -0400

    haciendo teacher/profile

---
+++ src/app/teacher/profile/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function TeacherProfile() {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">Perfil de Profesor</h1>
+        </div>
+    );
+}
