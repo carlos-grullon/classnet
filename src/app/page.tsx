721cf9d648d760fc2be6848a6ecf0904c090fbbc
NamikaseEXE <carlos0012010vegano@gmail.com>
2025-05-05 14:30:55 -0400

    pagina dashboard creada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,32 @@
-// import Image from "next/image";
+'use client';
+import Link from 'next/link';
+import { FaChalkboardTeacher } from 'react-icons/fa';
+import { PiStudentFill } from 'react-icons/pi';
 
-export default async function Home() {
-    return (
-    <>
-      <h1>Home</h1>
-      <p>Me gusta comer, tengo hambre</p>
-      <p>Me gusta comer, tengo hambre</p>
-      <p>No me gusta comer, no tengo hambre</p>
-      <button>Click me but not so hard</button>
-    </>
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4" style={{ background: 'var(--background)' }}>
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-8" style={{ color: 'var(--foreground)' }}>Bienvenido a ClassNet</h1>
+        <p className="mb-12" style={{ color: 'var(--foreground-muted)' }}>Por favor, selecciona tu tipo de usuario</p>
+        
+        <div className="flex flex-col sm:flex-row gap-6 justify-center">
+          <Link href="/teacher-login" 
+            className="flex items-center bg-blue-400 justify-center gap-3 px-8 py-4 
+            rounded-lg transform hover:scale-105 text-black transition-all duration-200 shadow-lg">
+            <FaChalkboardTeacher className="h-6 w-6" />
+            Profesor
+          </Link>
+
+          <Link href="/student-login"
+            className="flex items-center bg-amber-400 justify-center gap-3 px-8 
+            py-4 rounded-lg transform hover:scale-105 transition-all text-black
+            duration-200 shadow-lg">
+            <PiStudentFill className="h-6 w-6" />
+            Estudiante
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
