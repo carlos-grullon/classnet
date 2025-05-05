dde496e916c2f33b3ab3ac93ac0cc8d72cd887d3
alexioficial <grullonmatiasalexi@gmail.com>
2025-05-05 14:13:02 -0400

    hice el middleware

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+    const pathname = request.nextUrl.pathname;
+
+    if (
+        pathname.startsWith('/_next/static') ||
+        pathname.startsWith('/favicon.ico') ||
+        pathname.startsWith('/login') ||
+        pathname.startsWith('/register') ||
+        pathname.startsWith('/api/login') ||
+        pathname.startsWith('/api/register')
+    ) {
+        return NextResponse.next();
+    }
+    
+    const loginRedirect = NextResponse.redirect(new URL('/login', request.url));
+    
+    const sessionId = request.cookies.get('sessionId');
+    if (!sessionId) {
+        return loginRedirect;
+    }
+
+    return NextResponse.next();
+}
