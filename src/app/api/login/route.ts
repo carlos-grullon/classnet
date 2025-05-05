0dd97fd4f1ed70d68deef12bfa338d92b2b34c1a
alexioficial <grullonmatiasalexi@gmail.com>
2025-05-05 00:42:37 -0400

    puse las validaciones necesarias para que el login funcione

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -5,13 +5,17 @@ import { createSession } from "@/utils/Session";
 export async function POST(request: Request) {
     try {
         const data = await request.json();
-        const user = await Login(
+        const users = await Login(
             data.password,
-            data.email
+            data.email,
+            data.user_type || 'A'
         )
 
-        const idSession = await createSession('', user);
-        return NextResponse.json({ idSession: idSession });
+        if (users.length === 1) {
+            const idSession = await createSession('', users[0]);
+            return NextResponse.json({ idSession: idSession, TwoAccountsFound: false });
+        }
+        return NextResponse.json({ TwoAccountsFound: true })
     } catch (error) {
         if (error instanceof Error) {
             return NextResponse.json({ error: error.message }, { status: 500 });
