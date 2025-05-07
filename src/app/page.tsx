3dcf4bcef1b7ab3de6d77d47e4ff02dc3fa0b584
NamikaseEXE <carlos0012010vegano@gmail.com>
2025-05-06 23:28:32 -0400

    puse el texto mas bonito mientras se desarrolla

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@
 
 export default function Home() {
   return (
-    <>
-      <h1>Página en desarrollo</h1>
-    </>
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-6xl font-bold text-center bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent p-4">
+        ClassNet
+      </h1>
+      <p className="text-lg text-center text-gray-600">
+        Sistema de clases virtual para estudiantes y profesores, pronto disponible.
+      </p>
+    </div>
   );
 }
