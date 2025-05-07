299881347d8620299fbaf41958537ceb92e3b333
NamikaseEXE <carlos0012010vegano@gmail.com>
2025-05-06 23:39:50 -0400

    pequeño arreglo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h1 className="text-8xl font-bold text-center bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent p-4">
         ClassNet
       </h1>
-      <div className="text-lg text-center my-4 text-green-200">
+      <div className="text-3xl text-center my-4 text-green-200">
         Sistema de clases virtuales para estudiantes y profesores,
         <br />
         pronto disponible.
