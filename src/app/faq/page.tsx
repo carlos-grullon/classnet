e07d5a43b43d8b045d10edd882a78b56b8801811
NamikaseEXE <carlos0012010vegano@gmail.com>
2025-07-16 22:10:22 -0400

    mejora con el redirect con google hacia dentro de la app

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -226,7 +226,7 @@ export default function FAQPage() {
           >
             <Button
               className="bg-orange-500 hover:bg-orange-600 text-white px-8 py-4 text-lg font-semibold rounded-full shadow-lg"
-              onClick={() => window.location.href = "/register"}
+              onClick={() => window.location.href = "/login"}
             >
               ¡Inscríbete Ahora!
             </Button>
